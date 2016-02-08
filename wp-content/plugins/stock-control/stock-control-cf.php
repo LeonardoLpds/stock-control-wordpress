14637b7dcdfe1327df6bfcbd025b0d6f12f88499
Leonardo de Souza <leonardo.lpds@gmail.com>
2016-02-08 04:37:12 -0200

    Adding orders custom fields

--- wp-content/plugins/stock-control/stock-control-cf.php
+++ wp-content/plugins/stock-control/stock-control-cf.php
@@ -5,6 +5,9 @@ add_action('save_post', 'product_meta_save');
 add_action('add_meta_boxes', 'create_client_meta_box' );
 add_action('save_post', 'client_meta_save');
 
+add_action('add_meta_boxes', 'create_order_meta_box' );
+add_action('save_post', 'order_meta_save');
+
 /**
 * Creating products custom fields
 */
@@ -75,7 +78,7 @@ function product_meta_save($post_id) {
         update_post_meta($post_id, 'price', $_POST['price']);
     }
     if (isset($_POST["description"])) {
-        update_post_meta($post_id, 'description', sanitize_text_field($_POST['description']));
+        update_post_meta($post_id, 'description', $_POST['description']);
     }
 }
 
@@ -145,5 +148,84 @@ function client_meta_save($post_id) {
     }
     if (isset($_POST["phone"])) {
         update_post_meta($post_id, 'phone', sanitize_text_field($_POST['phone']));
+    }
+}
+
+/**
+* Creating orders custom fields
+*/
+function create_order_meta_box() {
+    add_meta_box(
+        'order_meta_box',
+        'orders Informations',
+        'order_meta_box_callback',
+        'order',
+        'normal'
+    );
+}
+
+function order_meta_box_callback($post) {
+    wp_nonce_field( basename(__FILE__), 'order_nonce' );
+    $order_stored_meta = get_post_meta( $post->ID );
+?>
+
+    <div>
+        <div class="meta-field-box">
+            <div class="meta-field-head">
+                <label for="product_id" class="meta-label-title">Product</label>
+            </div>
+            <div class="meta-field-content">
+                <select name="product_id" id="product_id">
+                    <option value="">--</option>
+                    <?php
+                    $products = get_posts(array('post_type' => 'product', 'post_status' => 'publish'));
+                    foreach ($products as $product):
+                    ?>
+                        <option value="<?=$product->ID?>" <?php selected($order_stored_meta['product_id'][0], $product->ID);?>><?=$product->post_title?></option>
+                    <?php
+                    endforeach;
+                    ?>
+                </select>
+            </div>
+        </div>
+        <div class="meta-field-box">
+            <div class="meta-field-head">
+                <label for="client_id" class="meta-label-title">Client</label>
+            </div>
+            <div class="meta-field-content">
+                <select name="client_id" id="client_id">
+                    <option value="">--</option>
+                    <?php
+                    $clients = get_posts(array('post_type' => 'client', 'post_status' => 'publish'));
+                    foreach ($clients as $client):
+                    ?>
+                        <option value="<?=$client->ID?>" <?php selected($order_stored_meta['client_id'][0], $client->ID);?>><?=$client->post_title?></option>
+                    <?php
+                    endforeach;
+                    ?>
+                </select>
+            </div>
+        </div>
+    </div>
+<?php
+}
+
+function order_meta_save($post_id) {
+    // Check save status
+    $is_autosave = wp_is_post_autosave($post_id);
+    $is_revision = wp_is_post_revision($post_id);
+    $is_valid_nonce = (isset($_POST['order_nonce']) && wp_verify_nonce($_POST['order_nonce'], basename(__FILE__))) ? true : false;
+
+    // Exit script depending on save status
+    if ($is_autosave || $is_revision || !$is_valid_nonce) {
+        return;
+    }
+
+    if (isset($_POST["product_id"])) {
+        update_post_meta($post_id, 'product_id', $_POST['product_id']);
+    }
+
+    if (isset($_POST["client_id"])) {
+        update_post_meta($post_id, 'client_id', $_POST['client_id']);
     }
 }
